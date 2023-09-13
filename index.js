4a80396e77c8c37eddc38e6cc76dc835ab5855a1
Devansh Purwar <75568315+devansh-purwar@users.noreply.github.com>
2023-09-14 00:40:45 +0530

    fixes for vercel

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import { generateUploadURL } from './s3.js'
+import path from 'path';
 
 const app = express()
+const __dirname = path.resolve();
+// app.use(express.static('front'))
 
-app.use(express.static('front'))
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.get('/s3Url', async (req, res) => {
   const url = await generateUploadURL()
